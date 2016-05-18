d0a352c22844c8cf1138f1dda91d7f9f642e9b66
Boris P. Hejblum <bhejblum@hsph.harvard.edu>
2016-05-18 01:20:45 +0000

    version 1.0.1

--- R/kernscr-package.R
+++ R/kernscr-package.R
@@ -6,8 +6,8 @@
 #'\tabular{ll}{
 #'Package: \tab kernscr\cr
 #'Type: \tab Package\cr
-#'Version: \tab 1.0.0\cr
-#'Date: \tab 2016-05-02\cr
+#'Version: \tab 1.0.1\cr
+#'Date: \tab 2016-05-03\cr
 #'License:\tab \href{http://www.gnu.org/licenses/gpl-2.0.txt}{GPL-2}\cr
 #'}
 #'The main function of the kernscr package is \code{\link{compute_all_tests}}
